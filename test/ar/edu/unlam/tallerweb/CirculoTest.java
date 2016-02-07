273b7e680ea3439a81d8ec2878027a21f65d6a54
arielrando <ariel_rando@hotmail.com.ar>
2016-02-07 20:37:02 -0300

    Modificacion: se agrego el test del circulo

--- test/ar/edu/unlam/tallerweb/CirculoTest.java
+++ test/ar/edu/unlam/tallerweb/CirculoTest.java
@@ -1,3 +1,35 @@
 package ar.edu.unlam.tallerweb;
 
-public class CirculoTest {}
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CirculoTest {
+	
+	@Test
+	public void calcularPerimetroDelCirculoTest (){
+		
+		Circulo miCirculo = new Circulo (5.0);
+		
+		Double esperado = 31.4;
+		
+		Double actual = miCirculo.calcularPerimetroCirculo();
+		
+		assertEquals (esperado.doubleValue(),actual.doubleValue(),0.01);
+		
+	}
+	
+	@Test
+	public void calcularAreaDelCirculo () {
+		
+		Circulo miCirculo = new Circulo (5.0);
+		
+		Double esperado = 78.5;
+		
+		Double actual = miCirculo.calcularAreaCirculo();
+		
+		assertEquals (esperado.doubleValue(),actual.doubleValue(),0.01); 
+		
+	}
+	
+}
