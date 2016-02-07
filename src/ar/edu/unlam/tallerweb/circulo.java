71769bed6c224e5dfb5106a3de4e86ffbb626b1b
Yami81 <yayunta@yahoo.com.ar>
2016-02-07 17:08:45 -0300

    Se crea la Clase Circulo

--- src/ar/edu/unlam/tallerweb/circulo.java
+++ src/ar/edu/unlam/tallerweb/circulo.java
@@ -1,4 +1,23 @@
 package ar.edu.unlam.tallerweb;
 
 public class circulo {
+	
+	private Double radio;
+	private Double pi = 3.14;
+	
+	public circulo(Double radio, Double pi){
+		this.radio = radio;
+		this.pi= pi;
+	}
+	
+	public Double calcularPerimetroCirculo(){
+		
+		return 2 * this.pi * this.radio;
+	}
+	
+	public Double calcularAreaCirculo(){
+		
+		return this.pi * Math.pow(this.radio, 2);
+		
+	}
 }
