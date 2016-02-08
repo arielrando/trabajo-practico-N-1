09d150807282374efa011ee5a7af4b6440aaede3
arielrando <ariel_rando@hotmail.com.ar>
2016-02-08 00:49:25 -0300

    Modificacion: Se agrego los test de fallo a los test del circulo

--- test/ar/edu/unlam/tallerweb/CirculoTest.java
+++ test/ar/edu/unlam/tallerweb/CirculoTest.java
@@ -1,6 +1,6 @@
 package ar.edu.unlam.tallerweb;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -29,6 +29,32 @@ public class CirculoTest {
 		Double actual = miCirculo.calcularAreaCirculo();
 		
 		assertEquals (esperado.doubleValue(),actual.doubleValue(),0.01); 
+		
+	}
+	
+	@Test
+	public void calcularPerimetroDelCirculoTestFallo (){
+		
+		Circulo miCirculo = new Circulo (5.0);
+		
+		Double esperado = 36.0;
+		
+		Double actual = miCirculo.calcularPerimetroCirculo();
+		
+		assertNotEquals (esperado.doubleValue(),actual.doubleValue());
+		
+	}
+	
+	@Test
+	public void calcularAreaDelCirculoFallo () {
+		
+		Circulo miCirculo = new Circulo (5.0);
+		
+		Double esperado = 80.0;
+		
+		Double actual = miCirculo.calcularAreaCirculo();
+		
+		assertNotEquals (esperado.doubleValue(),actual.doubleValue()); 
 		
 	}
 	
